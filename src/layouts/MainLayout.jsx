fc0c43373c575b0b960abd8c99704b6c149179e1
Car Gnee Admin <admin@car-gnee.com>
2026-06-16 15:31:27 +0530

    feat: complete pwa verification and production deployment

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,11 +2,13 @@ import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
+import { PWAInstallBanner } from "@/components/PWAInstallBanner";
 
 export function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <PWAInstallBanner />
       <main className="flex-grow">
         <Outlet />
       </main>
